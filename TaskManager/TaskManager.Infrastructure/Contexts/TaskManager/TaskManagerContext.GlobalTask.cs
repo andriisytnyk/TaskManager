a6c8796ced61e1923c91cf24f943fdf7b3612020
Andrii <sytnyk.andrii@gmail.com>
2020-06-16 23:44:10 +0300

    TaskContext was created

---
+++ TaskManager/TaskManager.Infrastructure/Contexts/TaskManager/TaskManagerContext.GlobalTask.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.DomainModel.Aggregates;
+using TaskManager.DomainModel.Common;
+
+namespace TaskManager.Infrastructure.Contexts.TaskManager
+{
+    public partial class TaskManagerContext
+    {
+        private const string GlobalTaskTableName = "GlobalTasks";
+
+        public DbSet<GlobalTask> GlobalTasks { get; set; }
+
+        public void ConfigureGlobalTask(EntityTypeBuilder<GlobalTask> builder)
+        {
+            builder.ToTable(TaskTableName);
+            builder.HasKey(g => g.Id).IsClustered();
+            builder.Property(g => g.Name).HasMaxLength(30).HasDefaultValue("New task").IsRequired();
+            builder.Property(g => g.Description).HasMaxLength(100);
+            builder.Property(g => g.Status).HasDefaultValue(Status.ToDo).IsRequired();
+            builder.Property(g => g.FinishDate).IsRequired();
+            builder.HasMany<Task>(TaskPrivatePropertyName)
+                .WithOne()
+                .HasForeignKey(ParentTaskName)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ TaskManager/TaskManager.Infrastructure/Contexts/TaskManager/TaskManagerContext.PlannedTask.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TaskManager.DomainModel.Aggregates;
+using TaskManager.DomainModel.Common;
+
+namespace TaskManager.Infrastructure.Contexts.TaskManager
+{
+    public partial class TaskManagerContext
+    {
+        private const string PlannedTaskTableName = "PlannedTasks";
+
+        public DbSet<GlobalTask> PlannedTasks { get; set; }
+
+        public void ConfigurePlannedTask(EntityTypeBuilder<PlannedTask> builder)
+        {
+            builder.ToTable(TaskTableName);
+            builder.HasKey(g => g.Id).IsClustered();
+            builder.Property(g => g.Name).HasMaxLength(30).HasDefaultValue("New task").IsRequired();
+            builder.Property(g => g.Description).HasMaxLength(100);
+            builder.Property(g => g.Status).HasDefaultValue(Status.ToDo).IsRequired();
+            builder.Property(g => g.StartDateTime).IsRequired();
+            builder.Property(g => g.FinishDateTime).IsRequired();
+            builder.Property(g => g.Estimation).IsRequired();
+            builder.Property(g => g.Requirement).HasDefaultValue(true).IsRequired();
+            builder.Property(g => g.Frequency).HasDefaultValue(Frequency.NonRepeating).IsRequired();
+            builder.HasMany<Task>(TaskPrivatePropertyName)
+                .WithOne()
+                .HasForeignKey(ParentTaskName)
+                .IsRequired()
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
