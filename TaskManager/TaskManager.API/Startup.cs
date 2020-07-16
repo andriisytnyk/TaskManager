19851a4a681eeacd0cb8f4cfd1bd0973c8a3b7b2
Andrii <sytnyk.andrii@gmail.com>
2020-07-16 22:46:50 +0300

    Changes

--- TaskManager/TaskManager.API/Startup.cs
+++ TaskManager/TaskManager.API/Startup.cs
@@ -1,24 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using TaskManager.API.Middleware;
 using TaskManager.Core.Messaging;
-using TaskManager.Core.Repositories;
-using TaskManager.DomainModel.Aggregates;
-using TaskManager.Infrastructure.Contexts.TaskManager;
 using TaskManager.Infrastructure.ExtensionMethods;
-using TaskManager.Infrastructure.PipelineBuilder;
-using TaskManager.Infrastructure.Repositories;
 
 namespace TaskManager
 {
